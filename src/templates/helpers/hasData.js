618750d6aecd180d1430f17d8513174505623e0c
Tom Gillard <tomgillar@gmail.com>
2016-09-11 01:32:56 +1000

    troubleshooting checking for optional partial data

---
+++ src/templates/helpers/hasData.js
@@ -0,0 +1,3 @@
+module.exports.hasData = function(obj1, obj2, options) {
+  return (!!obj1 || !!obj2) ? true : false;
+}
---
+++ src/templates/helpers/ifCond.js
@@ -0,0 +1,28 @@
+module.exports.ifCond = function (v1, operator, v2, options) {
+
+  if (arguments.length < 3) {
+      throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+  }
+
+  switch (operator) {
+      case '==':
+          return (v1 == v2) ? options.fn(this) : options.inverse(this);
+      case '===':
+          return (v1 === v2) ? options.fn(this) : options.inverse(this);
+      case '<':
+          return (v1 < v2) ? options.fn(this) : options.inverse(this);
+      case '<=':
+          return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+      case '>':
+          return (v1 > v2) ? options.fn(this) : options.inverse(this);
+      case '>=':
+          return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+      case '&&':
+          return (v1 && v2) ? options.fn(this) : options.inverse(this);
+      case '||':
+          return (v1 || v2) ? options.fn(this) : options.inverse(this);
+      default:
+          return options.inverse(this);
+  }
+
+};
